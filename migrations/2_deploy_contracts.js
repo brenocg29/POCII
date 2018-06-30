90e4f8c792b8177ad1ba2f9a99f9e44840c07873
brenocg29 <gabrichbreno@gmail.com>
2018-06-30 14:00:09 -0300

    a

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
-var Adoption = artifacts.require("Adoption");
+
 var Ballot = artifacts.require("Ballot");
 module.exports = function(deployer){
-
-	deployer.deploy(Adoption);
 	deployer.deploy(Ballot);
 
 };
